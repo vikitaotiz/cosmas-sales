fe093070fcaf8f6e57721e22089714ce566dadf0
vikitaotiz <victorotieno598@gmail.com>
2023-07-21 17:36:51 +0300

    sales1

---
+++ assets/helpers.a08a01a1.js
@@ -0,0 +1,1 @@
+const s="https://cosmas.missnadhifu.com/laravel/public/api/v1";export{s as b};
---
+++ assets/vuequery.edafcfb3.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.efe89fb5.js";import"./index.d831531d.js";var o=({app:e})=>{e.use(r)};export{o as default};
