932592d583ffe190164df6f34083d7144f888469
vikitaotiz <victorotieno598@gmail.com>
2023-08-05 08:58:53 +0300

    sales12

---
+++ assets/use-dark.d9cdc187.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index.8a5a258d.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
---
+++ assets/vuequery.905ad92a.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.b66eac6f.js";import"./index.8a5a258d.js";var o=({app:e})=>{e.use(r)};export{o as default};
